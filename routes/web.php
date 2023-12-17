313a3916121e54a10f01238e660ee097604ca9b5
Koyama23 <v.tigers.1019genki@gmail.com>
2023-12-17 16:01:15 +0900

    karikyuramu8-4

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,9 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', [PostController::class,'index']);   
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts', [PostController::class, 'store']);
+
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
